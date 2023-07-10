2cfff84630bd1f017843c62133d5f284d4d8ecc4
ec98 <carlospiruch98@gmail.com>
2023-07-09 23:35:42 -0500

    Deber second fragment marvel buscador & layout second fragment 09/07/2023

--- app/src/main/java/com/example/dispositivosmoviles/ui/ui/fragment/SecondFragment.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/ui/fragment/SecondFragment.kt
@@ -1,17 +1,32 @@
 package com.example.dispositivosmoviles.ui.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dispositivosmoviles.R
-import com.example.dispositivosmoviles.databinding.FragmentFirstBinding
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dispositivosmoviles.databinding.FragmentSecondBinding
+import com.example.dispositivosmoviles.ui.logic.data.marvelCharacters
+import com.example.dispositivosmoviles.ui.logic.jikan_logic.JikanAnimeLogic
+import com.example.dispositivosmoviles.ui.logic.marvel_logic.MarvelLogic
+import com.example.dispositivosmoviles.ui.ui.activities.DetailsMarvelItem
+import com.example.dispositivosmoviles.ui.ui.fragment.adapters.MarvelAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SecondFragment : Fragment() {
 
-    private lateinit var binding : FragmentSecondBinding
+    private lateinit var binding: FragmentSecondBinding
+    private lateinit var lmanager: LinearLayoutManager
+    private lateinit var rvAdapter: MarvelAdapter
+
+    private var marvelCharsItems: MutableList<marvelCharacters> = mutableListOf<marvelCharacters>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +34,67 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSecondBinding.inflate(
-            layoutInflater,container,false
+            layoutInflater, container, false
+        )
+        lmanager = LinearLayoutManager(
+            requireActivity(), LinearLayoutManager.VERTICAL, false
         )
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //carga los datos
+//        chargeDataRV("cap")
+
+        binding.rvSwipe.setOnRefreshListener {
+//            chargeDataRV("cap")
+            binding.rvSwipe.isRefreshing = false
+        }
+        binding.rvMarvelChars.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (dy > 0) {
+                    val v = lmanager.childCount
+                    val p = lmanager.findFirstVisibleItemPosition()
+                    val t = lmanager.itemCount
+                    if ((v + p) >= t) {
+                        lifecycleScope.launch((Dispatchers.IO)) {
+                            val newItems = JikanAnimeLogic().getAllanimes()
+                            withContext(Dispatchers.Main) {
+                                rvAdapter.updateListItems(newItems)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        //nos sirve para filtrar informacion
+        binding.textfilder.addTextChangedListener {
+            chargeDataRV(binding.textfilder.text.toString())
+        }
+    }
+
+    private fun sendMarvelItem(item: marvelCharacters) {
+        val i = Intent(requireActivity(), DetailsMarvelItem::class.java)
+        i.putExtra("name", item)
+        startActivity(i)
+    }
+    private fun chargeDataRV(search: String) {
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            marvelCharsItems = withContext(Dispatchers.IO) {
+                return@withContext (MarvelLogic().getMarvelChars(
+                    search, 99
+                ))
+            }
+            rvAdapter = MarvelAdapter(marvelCharsItems, fnClick = { sendMarvelItem(it) })
+            binding.rvMarvelChars.apply {
+                this.adapter = rvAdapter
+                this.layoutManager = lmanager
+            }
+        }
     }
 }
